26ed40a59fd49266129aae8a88ace852229e591f
Alexander Khiora <sanyabk1@gmail.com>
2018-06-17 23:41:01 +0300

    Add fake API response.

---
+++ fakeApi.js
@@ -0,0 +1,24 @@
+export const fakeEventsResponse = [
+    {
+        fields: {
+            cityName: "ODESSA",
+            cover: [
+                {
+                    url: "https://dl.airtable.com/wpF5oUaZR0uoqBlKsDzL_bsDbgnerQlGO4ckT4ioe_full_tkavlbk.jpg"
+                }
+            ],
+            description: "<p>Проект допомагає літнім людям продуктовими наборами та простою увагою, для самотніх літніx людей будь-яка турбота — справжня подія.</p>",
+            endsOn: "2018-07-16T14:00:00.000Z",
+            eventType: "VOLUNTEERING",
+            id: 14,
+            onSite: true,
+            placeHref: "https://goo.gl/maps/AVG8oti84kD2",
+            placeName: "Штаб (Жуковського, 21)",
+            priceInfo: "FREE",
+            startsOn: "2018-07-16T09:00:00.000Z",
+            title: "Добрий Гість",
+            urlAbout: "http://volunteer.country/dobriygist",
+            urlToAttend: "http://volunteer.country/dobriygist"
+        }
+    }
+]
